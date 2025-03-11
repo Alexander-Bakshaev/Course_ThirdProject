9ef5fa97726bde0e2234ff46b475118dc1d0a62a
Alexander-Bakshaev <shuriknt@gmail.com>
2025-03-11 21:19:10 +0500

    Создана первая фома

--- crud/forms.py
+++ crud/forms.py
@@ -2,5 +2,14 @@ from django import forms
 
 
 class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
+    name = forms.CharField(label='Имя')
+    age = forms.IntegerField(label='Возраст', error_messages={
+        'required': 'Поле обязательно для заполнения',
+        'max_value': 'Возраст не может быть больше 100',
+        'min_value': 'Возраст не может быть меньше 0',
+    })
+
+class ProductForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    description = forms.CharField(max_length=255)
+    price = forms.IntegerField()
